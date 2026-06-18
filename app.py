a5fe7d44d3f92df8f1afbc4ed36764c193454966
rainbowzie8-star <rainbowzie8@gmail.com>
2026-06-18 18:10:00 +0700

    Menambahkan folder templates dan memperbaiki struktur file

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import pickle
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "<h1>Halo! Ini web Flask pertama Dewangga!</h1>"
+# pastikan file ini ada di folder yang sama: model.pkl & scaler.pkl
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)  # diasumsikan: [Decision Tree, SVC]
+with open('scaler.pkl', 'rb') as f:
+    scaler = pickle.load(f)
 
-if __name__ == "__main__":
+model_names = ['Decision Tree', 'SVC']
+
+@app.route('/')
+def index():
+    return render_template('index.html', model_names=model_names)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = {
+        'Pregnancies': int(request.form['pregnancies']),
+        'Glucose': int(request.form['glucose']),
+        'BloodPressure': int(request.form['blood_pressure']),
+        'SkinThickness': int(request.form['skin_thickness']),
+        'Insulin': int(request.form['insulin']),
+        'BMI': float(request.form['bmi']),
+        'DiabetesPedigreeFunction': float(request.form['diabetes_pedigree']),
+        'Age': int(request.form['age'])
+    }
+    df = pd.DataFrame(data, index=[0])
+    X = scaler.transform(df)
+
+    clf = model[ model_names.index(request.form['model']) ]
+    y = clf.predict(X)
+    prediction = 'Diabetic' if int(y[0]) == 1 else 'Non-Diabetic'
+
+    return render_template('index.html', model_names=model_names, prediction=prediction)
+
+if __name__ == '__main__':
     app.run(debug=True)
